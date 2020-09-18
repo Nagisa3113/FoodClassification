daac4506792a14d8a78d1e44aa59229c1645f1c8
nagisa <nagisa@nagisas-MacBook-Pro.local>
2020-09-18 17:14:14 +0800

    revise data

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Embedding, Flatten, Dense, SimpleRNN, LSTM, Bidirectional
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 
 is_train = True
 
-food = ('水煮肉',
+food = ('水煮肉片',
         '糖醋排骨',
-        '排骨莲藕',
+        '排骨莲藕汤',
         '黄金虾仁炒饭',
         '锅包肉',
         '薯条',
@@ -23,6 +23,25 @@ food = ('水煮肉',
         '宫保鸡丁',
         '番茄紫菜蛋汤',
         )
+
+os_dir = '/Users'
+# os_dir='/home'
+base_dir = os_dir + '/nagisa/PycharmProjects/FC'
+
+maxlen = 10
+training_samples = 2400
+validation_samples = 100
+max_words = 2000
+embedding_dim = 50
+
+
+def stop(texts, lang='english'):
+    stopset = set(stopwords.words(lang))
+    for i, sentences in enumerate(texts):
+        a = ''.join(sentences)
+        new_sent = [word for word in a.split() if word not in stopset]
+        texts[i] = ' '.join(new_sent)
+    return texts
 
 
 def to_one_hot(labels, dimension=10):
@@ -33,104 +52,52 @@ def to_one_hot(labels, dimension=10):
     return labels
 
 
-os_dir = '/Users'
-# os_dir='/home'
-base_dir = os_dir + '/nagisa/PycharmProjects/FC'
+def load_data(data_dir, repeat=1):
+    texts = []
+    labels = []
+    for i in range(repeat):
+        for i in range(10):
+            f = open(os.path.join(data_dir, str(i) + '.txt'), encoding='utf-8')
+            lines = f.readlines()
+            for line in lines:
+                texts.append(line)
+                labels.append(i)
+            f.close()
+    return texts, labels
 
-sw = set(stopwords.words('english'))
-
-labels = []
-texts = []
 
 train_dir = os.path.join(base_dir, 'train')
-
-for i in range(9):
-    ddir = '0' + str(i + 1) + '.txt'
-    f = open(os.path.join(train_dir, ddir))
-    lines = f.readlines()
-    for line in lines:
-        texts.append(line)
-        labels.append(i)
-    f.close()
-
-ddir = '10.txt'
-f = open(os.path.join(train_dir, ddir))
-lines = f.readlines()
-for line in lines:
-    texts.append(line)
-    labels.append(9)
-f.close()
-
-for i in range(9):
-    ddir = '0' + str(i + 1) + '.txt'
-    f = open(os.path.join(train_dir, ddir))
-    lines = f.readlines()
-    for line in lines:
-        texts.append(line)
-        labels.append(i)
-    f.close()
-
-ddir = '10.txt'
-f = open(os.path.join(train_dir, ddir))
-lines = f.readlines()
-for line in lines:
-    texts.append(line)
-    labels.append(9)
-f.close()
-
-labels = to_one_hot(labels)
-
-maxlen = 10
-training_samples = 2400
-validation_samples = 100
-max_words = 1000
-
-
-def stop(texts):
-    for i, sentences in enumerate(texts):
-        a = ''.join(sentences)
-        new_sent = [word for word in a.split() if word not in sw]
-        texts[i] = ' '.join(new_sent)
-    return texts
+test_dir = os.path.join(base_dir, 'test')
+texts, labels = load_data(train_dir, repeat=4)
 
 texts = stop(texts)
+labels = to_one_hot(labels)
 
 tokenizer = Tokenizer(num_words=max_words)
 tokenizer.fit_on_texts(texts)
 sequences = tokenizer.texts_to_sequences(texts)
-
 word_index = tokenizer.word_index
 print('Found %s unique tokens.' % len(word_index))
 data = pad_sequences(sequences, maxlen=maxlen)
 labels = np.asarray(labels)
 print('Shape of data tensor:', data.shape)
 print('Shape of label tensor:', labels.shape)
-
 indices = np.arange(data.shape[0])
 np.random.shuffle(indices)
 data = data[indices]
 labels = labels[indices]
 
-x_train = data[:training_samples]
-y_train = labels[:training_samples]
-x_val = data[training_samples:training_samples + validation_samples]
-y_val = labels[training_samples:training_samples + validation_samples]
-
-db_train = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(4)
-
 glove_dir = os.path.join(base_dir, 'glove')
 embeddings_index = {}
-f = open(os.path.join(glove_dir, 'glove.6B.50d.txt'))
+f = open(os.path.join(glove_dir, 'glove.6B.50d.txt'), encoding='utf-8')
 for line in f:
     values = line.split()
     word = values[0]
     coefs = np.asarray(values[1:], dtype='float32')
     embeddings_index[word] = coefs
 f.close()
-
 print('Found %s word vectors.' % len(embeddings_index))
 
-embedding_dim = 50
 embedding_matrix = np.zeros((max_words, embedding_dim))
 for word, i in word_index.items():
     if i < max_words:
@@ -141,13 +108,16 @@ for word, i in word_index.items():
 print(embedding_matrix.shape)
 print(embedding_matrix)
 
+# x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True)
+# db_train = tf.data.Dataset.from_tensor_slices((data, labels)).batch(16)
+
 model = Sequential()
 model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
 # model.add(SimpleRNN(32, dropout=0.1, return_sequences=True))
 # model.add(SimpleRNN(32, dropout=0.1))
 # model.add(Dense(64))
 # model.add(LSTM(32, return_sequences=True))
-model.add(LSTM(32, dropout=0.1))
+model.add(LSTM(48, dropout=0.1))
 # model.add(Dense(32))
 # model.add(Dense(1, activation='sigmoid'))
 model.add(Dense(10, activation='softmax'))
@@ -169,35 +139,27 @@ model.compile(optimizer='rmsprop',
 #                         # validation_split=0.2,
 #                         validation_data=(x_train[test], y_train[test]))
 if is_train:
-    history = model.fit(x_train, y_train,
-                        epochs=20, batch_size=16,
-                        validation_data=(x_val, y_val)
+    history = model.fit(data, labels,
+                        epochs=10,
+                        batch_size=64,
+                        validation_split=0.2
+                        # validation_data=(x_test, y_test)
                         )
 
     model.save_weights('pre.h5')
 
-
-
-test_dir = os.path.join(base_dir, 'test')
+# test_dir = os.path.join(base_dir, 'test')
 labels = []
 texts = []
-for label_type in ['01.txt', '02.txt']:
-    fn = os.path.join(test_dir, label_type)
-    f = open(fn)
-    lines = f.readlines()
-    for line in lines:
-        texts.append(line)
-        if label_type == '01.txt':
-            labels.append(0)
-        else:
-            labels.append(1)
-    f.close()
+texts, labels = load_data(test_dir)
+texts = stop(texts)
+labels = to_one_hot(labels)
 
 sequences = tokenizer.texts_to_sequences(texts)
 x_test = pad_sequences(sequences, maxlen=maxlen)
 y_test = np.asarray(labels)
 
-# print(model.evaluate(x_test, y_test))
+print(model.evaluate(x_test, y_test))
 # print(model(x_test))
 
 
